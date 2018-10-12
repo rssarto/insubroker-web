1d2445a2056d50d074b9b2354f65880944b9760b
rssarto <ricardo.soares.sarto@gmail.com>
2018-10-12 20:11:25 -0300

    telefone validation

--- src/app/shared/validators/telefone.validator.ts
+++ src/app/shared/validators/telefone.validator.ts
@@ -2,13 +2,15 @@ import { AbstractControl } from '@angular/forms';
 import * as fromUtilFunctions from '@app/shared/util.function';
 
 export function validateTelefone(control: AbstractControl) {
-  if ( fromUtilFunctions.isCelular(control.value) ) {
-    if ( !fromUtilFunctions.isValidCelular(control.value) ) {
-      return {'invalidCelular' : true};
-    }
-  } else {
-    if ( !fromUtilFunctions.isValidTelefoneFixo(control.value) ) {
-      return {'invalidTelefoneFixo': true};
+  if ( control.value.trim() !== '' ) {
+    if  ( fromUtilFunctions.isCelular(control.value) ) {
+      if ( !fromUtilFunctions.isValidCelular(control.value) ) {
+        return {'invalidCelular' : true};
+      }
+    } else {
+      if ( !fromUtilFunctions.isValidTelefoneFixo(control.value) ) {
+        return {'invalidTelefoneFixo': true};
+      }
     }
   }
   return null;
