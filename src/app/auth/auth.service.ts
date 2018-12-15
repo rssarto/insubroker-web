ebee633a351b2a8e03d53900f681365f82857bbd
rssarto <ricardo.soares.sarto@gmail.com>
2018-12-15 17:12:31 -0200

    validating email in the backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,10 @@
 import { Usuario } from '@app/shared/model/usuario.model';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Credential } from '@app/auth/model/credential.model';
-import { SessionState } from '@app/auth/store/session.store';
 import { map } from 'rxjs/operators';
 import { environment } from '@env/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-const cadastros: Usuario[] = [
-  new Usuario('RICARDO SOARES SARTO', '123456', 'ricardo.soares.sarto@gmail.com')
-];
 
 @Injectable()
 export class AuthService {
@@ -17,27 +12,10 @@ export class AuthService {
   }
 
   cadastrar(cadastro: Usuario) {
-    /*
-    const actionPromise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        cadastros.push(cadastro);
-        resolve();
-      }, 500);
-    });
-    return of(actionPromise);
-    */
    return this.httpClient.post(environment.resources.person, cadastro);
   }
 
   find(email: string): Observable<Usuario> {
-    /*
-    let cadastro: Usuario = null;
-    const findIndex = cadastros.findIndex((value: Usuario) => value.login === email);
-    if ( findIndex !== -1 ) {
-      cadastro = cadastros[findIndex];
-    }
-    return of(cadastro);
-    */
     return this.httpClient.get<Usuario>(environment.resources.person + '/email/' + email);
   }
 
